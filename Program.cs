b32f6614d10e677e6f923f8c570edefb5684de54
Sibanbis <104072012+Sibanbis@users.noreply.github.com>
2022-05-28 12:07:34 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+
+namespace FirstApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {           
+                var builder = new ConfigurationBuilder()                
+                .SetBasePath(Directory.GetCurrentDirectory())               
+                .AddJsonFile("appsettings.json");
+                var config = builder.Build();
+
+                string connectionString = config.GetConnectionString("DefaultConnection");
+
+                var optionsBuilder = new DbContextOptionsBuilder<helloappdbContext>();
+                var options = optionsBuilder.UseSqlServer(connectionString).Options;
+                using (helloappdbContext db = new helloappdbContext())
+                {
+                    var users = db.Users.ToList();
+                    foreach (User u in users)
+                    {
+                        Console.WriteLine($"{u.Id}.{u.Name} - {u.Age}");
+                    }
+                }
+                Console.Read();
+            }
+        }
+    }
---
+++ helloappdbContext.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace FirstApp
+{
+    public partial class helloappdbContext : DbContext
+    {
+        public helloappdbContext()
+        {
+        }
+
+        public helloappdbContext(DbContextOptions<helloappdbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<User> Users { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+             optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=helloappdb;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
